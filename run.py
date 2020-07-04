bed33e6643e41bee5b9f98dcc047c3b74d797c7d
JDmocho <joanna.dmocho@gmail.com>
2020-07-04 23:02:44 +0200

    Fix: change name for future tests

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import unittest
 from tests.register_private_page_test import RegistrationPrivateTest
-#from tests.register_company_page_test import RegistrationCompanyTest
-#from tests.register_private_page_test import RegistrationPrivateTest
+#from tests.register_company_page_test import LoginPageTest
+#from tests.register_private_page_test import CartPageTest
 
 
 # Pobierz testy, które chcesz uruchomić
 registration_test_1 = unittest.TestLoader().loadTestsFromTestCase(RegistrationPrivateTest)
-#registration_test_2 = unittest.TestLoader().loadTestsFromTestCase(RegistrationOrganizationTest)
-#registration_test_3 = unittest.TestLoader().loadTestsFromTestCase(RegistrationOrganizationTest)
+#registration_test_2 = unittest.TestLoader().loadTestsFromTestCase(LoginPageTest)
+#registration_test_3 = unittest.TestLoader().loadTestsFromTestCase(CartPageTest)
 
 # Stwórz Test Suita łączac testy
 test_suite = unittest.TestSuite([registration_test_1])
