8eff180aaebc0b2224baa465ca7ee2f6c148f88e
JDmocho <joanna.dmocho@gmail.com>
2020-07-09 18:11:29 +0200

    fix: add missing methods

--- tests/login_form.py
+++ tests/login_form.py
@@ -19,6 +19,21 @@ CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 class LoginForm(BaseTest):
     """Testing the login form."""
 
+    def test_correct_data(self):
+        """Register with correct email should be successful."""
+
+        self.driver.find_element(*LoginPageLocators.LOG_IN_BTN).click()
+        self.driver.find_element(*LoginPageLocators.FILL_EMAIL).send_keys("joanna.software.tester@gmail.com")
+        self.driver.find_element(*LoginPageLocators.FILL_PASS).send_keys("Tester2020")
+        self.driver.find_element(*LoginPageLocators.SIGN_IN_BTN).submit()
+        user_name = self.driver.find_element(*LoginPageLocators.USER_NAME).text
+        self.assertEqual(user_name, "JOANNA")
+        self.driver.find_element(*LoginPageLocators.USER_NAME).click()
+        self.driver.find_element(*LoginPageLocators.LOG_OUT).click()
+        log_in = self.driver.find_element(*LoginPageLocators.LOG_IN_BTN).text
+        self.assertEqual(log_in, "LOGOWANIE")
+
+
     def test_empty_login_form(self):
         """Register with empty data should fail."""
 
@@ -26,7 +41,7 @@ class LoginForm(BaseTest):
         self.driver.find_element(*LoginPageLocators.FILL_EMAIL).send_keys("")
         self.driver.find_element(*LoginPageLocators.FILL_PASS).send_keys("")
         self.driver.find_element(*LoginPageLocators.SIGN_IN_BTN).submit()
-        self.driver.find_element(LoginPageLocators.ERROR_EL_LOGIN)
+        self.driver.find_element(*LoginPageLocators.ERROR_EL_LOGIN)
         error_elements = self.driver.find_elements(*LoginPageLocators.ERROR_EL_LOGIN)
 
         for el in error_elements:
@@ -71,23 +86,11 @@ class LoginForm(BaseTest):
         self.driver.find_element(*LoginPageLocators.LOG_IN_BTN).click()
         self.driver.find_element(*LoginPageLocators.FILL_EMAIL).send_keys("joanna@joanna.pl")
         self.driver.find_element(*LoginPageLocators.FILL_PASS).send_keys(password)
-        self.driver(LoginPageLocators.SIGN_IN_BTN).submit()
+        self.driver.find_element(*LoginPageLocators.SIGN_IN_BTN).click()
         error_text = self.driver.find_element(*LoginPageLocators.ERROR_El2).text
         self.assertEqual(error_text, "DANE DOSTĘPU SĄ NIEPRAWIDŁOWE")
 
-    def test_correct_data(self):
-        """Register with correct email should be successful."""
 
-        self.driver.find_element(*LoginPageLocators.LOG_IN_BTN).click()
-        self.driver.find_element(*LoginPageLocators.FILL_EMAIL).send_keys("joanna.software.tester@gmail.com")
-        self.driver.find_element(*LoginPageLocators.FILL_PASS).send_keys("Tester2020")
-        self.driver.find_element(*LoginPageLocators.SIGN_IN_BTN).submit()
-        user_name = self.driver.find_element(*LoginPageLocators.USER_NAME).text
-        self.assertEqual(user_name, "JOANNA")
-        self.driver.find_element(*LoginPageLocators.USER_NAME).click()
-        self.driver(*LoginPageLocators.LOG_OUT).click()
-        log_in = self.driver.find_element(*LoginPageLocators.LOG_IN_BTN).text
-        self.assertEqual(log_in, "LOGOWANIE")
 
 
 if __name__=="__main__":
