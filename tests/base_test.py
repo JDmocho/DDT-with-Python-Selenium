dae3ddd2970d6efd0ae67da33974d9de6a822352
JDmocho <joanna.dmocho@gmail.com>
2020-07-09 09:55:40 +0200

    fix: add delete_all_cookies method

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,15 +1,17 @@
 import unittest
 from selenium import webdriver
 
+
 class BaseTest(unittest.TestCase):
-    """
-    Klasa bazowa każdego testu
-    """
+    """Base test class"""
+
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.get('https://www.zara.com/pl/')
         self.driver.maximize_window()
         self.driver.implicitly_wait(30)
+        self.driver.delete_all_cookies()
+
 
     def tearDown(self):
         self.driver.quit()
